33d11695adb0dc8dd0edbe150a3a4e2eea5599f0
ihccenen <wirkz01@gmail.com>
2023-08-31 19:09:07 -0300

    added publish and delete post

--- src/components/PostPreview.tsx
+++ src/components/PostPreview.tsx
@@ -1,14 +1,58 @@
 import { Link } from 'react-router-dom';
+import { useMutation, useQueryClient } from '@tanstack/react-query';
 import moment from 'moment';
+import { deletePost, updatePostPublishedStatus } from '../api/posts';
 
 export default function PostPreview({ post }: { post: IPost }) {
+  const queryClient = useQueryClient();
+  const postPublishedMutation = useMutation({
+    mutationFn: updatePostPublishedStatus,
+    onSuccess: () => queryClient.invalidateQueries({ queryKey: ['posts'] }),
+  });
+  const postDeleteMutation = useMutation({
+    mutationFn: deletePost,
+    onSuccess: () => queryClient.invalidateQueries({ queryKey: ['posts'] }),
+  });
+
+  const handlePublishClick = () => {
+    postPublishedMutation.mutate({
+      published: !post.published,
+      postId: post._id,
+    });
+  };
+
+  const handleDeleteClick = () => {
+    postDeleteMutation.mutate({
+      postId: post._id,
+    });
+  };
+
   return (
-    <div className="post-wrapper flex">
-      <div className="flex">
-        <h2>
-          <Link to={`/posts/${post._id}`}>{post.title}</Link>
-        </h2>
-        <p>Posted by: {post.user.username}</p>
+    <div
+      className={`${
+        post.published ? 'published' : 'unpublished'
+      } post-wrapper flex`}
+    >
+      <div className="post-top-wrapper flex">
+        <div className="flex">
+          <h2>
+            <Link to={`/posts/${post._id}`}>{post.title}</Link>
+          </h2>
+          <p>Posted by: {post.user.username}</p>
+        </div>
+
+        <div className="flex">
+          <button
+            className={`published-status-btn ${
+              post.published ? 'unpublish-btn' : 'publish-btn'
+            }`}
+            type="button"
+            onClick={handlePublishClick}
+            disabled={postPublishedMutation.isLoading}
+          >
+            {post.published ? 'Unpublish' : 'Publish'} Post
+          </button>
+        </div>
       </div>
       <div className="post-text">
         <p>
@@ -22,8 +66,18 @@ export default function PostPreview({ post }: { post: IPost }) {
           )}
         </p>
       </div>
-      <div className="posted-at">
-        <p>{moment(post.createdAt).format('MMM Do YY')}</p>
+      <div className="post-bottom-wrapper flex">
+        <button
+          className="delete-post-btn"
+          type="button"
+          onClick={handleDeleteClick}
+          disabled={postDeleteMutation.isLoading}
+        >
+          Delete Post
+        </button>
+        <div>
+          <p>{moment(post.updatedAt).format('MMM Do YY')}</p>
+        </div>
       </div>
     </div>
   );
