1fb6a3d5542acca98c331b1da1f8971678aa9e72
ihccenen <wirkz01@gmail.com>
2023-08-31 19:08:56 -0300

    added publish status and delete post

--- src/components/Post.tsx
+++ src/components/Post.tsx
@@ -1,12 +1,20 @@
-import { useQuery } from '@tanstack/react-query';
+import { useNavigate } from 'react-router-dom';
+import { useMutation, useQuery, useQueryClient } from '@tanstack/react-query';
 import { useParams } from 'react-router-dom';
-import { getSinglePost } from '../api/posts';
+import {
+  deletePost,
+  getSinglePost,
+  updatePostPublishedStatus,
+} from '../api/posts';
 import { getPostComments } from '../api/comments';
 import Comments from './Comments';
 import moment from 'moment';
 
 export default function Post() {
   const { postId = '' } = useParams();
+  const navigate = useNavigate();
+
+  const queryClient = useQueryClient();
 
   const postQuery = useQuery({
     queryKey: ['posts', postId],
@@ -17,6 +25,30 @@ export default function Post() {
     queryKey: ['posts', postId, 'comments'],
     queryFn: () => getPostComments(postId),
   });
+
+  const postPublishedMutation = useMutation({
+    mutationFn: updatePostPublishedStatus,
+    onSuccess: () => queryClient.invalidateQueries({ queryKey: ['posts'] }),
+  });
+
+  const postDeleteMutation = useMutation({
+    mutationFn: deletePost,
+    onSuccess: () => queryClient.invalidateQueries({ queryKey: ['posts'] }),
+  });
+
+  const handlePublishClick = () => {
+    postPublishedMutation.mutate({
+      published: !postQuery.data.published,
+      postId: postQuery.data._id,
+    });
+  };
+
+  const handleDeleteClick = () => {
+    postDeleteMutation.mutate({
+      postId: postQuery.data._id,
+    });
+    navigate('/');
+  };
 
   if (postQuery.isLoading) {
     return (
@@ -36,13 +68,44 @@ export default function Post() {
 
   return (
     <main className="main flex">
-      <div className="post-wrapper">
-        <h1>{postQuery.data.title}</h1>
+      <div
+        className={`${
+          postQuery.data.published ? 'published' : 'unpublished'
+        } post-wrapper`}
+      >
+        <div className="post-top-wrapper flex">
+          <div className="flex">
+            <h1>{postQuery.data.title}</h1>
+            <p>Posted by: {postQuery.data.user.username}</p>
+          </div>
+
+          <div className="flex">
+            <button
+              className={`published-status-btn ${
+                postQuery.data.published ? 'unpublish-btn' : 'publish-btn'
+              }`}
+              type="button"
+              onClick={handlePublishClick}
+              disabled={postPublishedMutation.isLoading}
+            >
+              {postQuery.data.published ? 'Unpublish' : 'Publish'} Post
+            </button>
+
+            <button
+              className="delete-post-btn"
+              type="button"
+              onClick={handleDeleteClick}
+              disabled={postDeleteMutation.isLoading}
+            >
+              Delete Post
+            </button>
+          </div>
+        </div>
         <div className="post-text">
           <p>{postQuery.data.text}</p>
         </div>
         <div className="posted-at">
-          <p>{moment(postQuery.data.createdAt).format('MMM Do YY')}</p>
+          <p>{moment(postQuery.data.updatedAt).format('MMM Do YY')}</p>
         </div>
       </div>
       {commentsQuery.isLoading ? (
@@ -50,7 +113,7 @@ export default function Post() {
       ) : commentsQuery.isError && commentsQuery.error instanceof Error ? (
         <p>{commentsQuery.error.message}</p>
       ) : (
-        <Comments comments={commentsQuery.data} />
+        <Comments comments={commentsQuery.data} postId={postId}/>
       )}
     </main>
   );
