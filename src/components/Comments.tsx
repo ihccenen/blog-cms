9582c4205531330d0a7504e3589474af9197ca6e
ihccenen <wirkz01@gmail.com>
2023-08-31 19:08:08 -0300

    added delete comment

--- src/components/Comments.tsx
+++ src/components/Comments.tsx
@@ -1,7 +1,31 @@
+import { useMutation, useQueryClient } from '@tanstack/react-query';
 import CommentForm from './CommentForm';
 import moment from 'moment';
+import { deletePostComment } from '../api/comments';
 
-export default function Comments({ comments }: { comments: IComment[] }) {
+export default function Comments({
+  comments,
+  postId,
+}: {
+  comments: IComment[];
+  postId: string;
+}) {
+  const queryClient = useQueryClient();
+  const deleteCommentMutation = useMutation({
+    mutationFn: deletePostComment,
+    onSuccess: () =>
+      queryClient.invalidateQueries({
+        queryKey: ['posts', postId, 'comments'],
+      }),
+  });
+
+  const handleClick = (commentId: string) => {
+    deleteCommentMutation.mutate({
+      commentId,
+      postId,
+    });
+  };
+
   return (
     <div className="comments flex">
       <h2>Comments:</h2>
@@ -13,6 +37,12 @@ export default function Comments({ comments }: { comments: IComment[] }) {
             <p>{username}:</p>
             <p>{text}</p>
             <p className="posted-at">{moment(createdAt).format('MMM Do YY')}</p>
+            <button
+              className="delete-comment-btn"
+              onClick={() => handleClick(_id)}
+            >
+              Delete Comment
+            </button>
           </div>
         ))
       )}
