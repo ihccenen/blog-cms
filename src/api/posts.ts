e9e2da09f31a1797d9e988f21c663302f5ddca39
ihccenen <wirkz01@gmail.com>
2023-08-31 19:07:55 -0300

    added create and update published post

--- src/api/posts.ts
+++ src/api/posts.ts
@@ -21,3 +21,64 @@ export async function getSinglePost(postId: string) {
 
   return result;
 }
+
+export async function updatePostPublishedStatus({
+  published,
+  postId,
+}: {
+  published: boolean;
+  postId: string;
+}) {
+  const post = await fetch(`${import.meta.env.VITE_API_URL}/posts/${postId}`, {
+    method: 'PUT',
+    credentials: 'include',
+    body: JSON.stringify({ published }),
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  });
+
+  const result = await post.json();
+
+  if (!post.ok) throw new Error(result.message);
+
+  return result;
+}
+
+export async function createPost({
+  title,
+  text,
+  published,
+}: {
+  title: string;
+  text: string;
+  published: boolean;
+}) {
+  const post = await fetch(`${import.meta.env.VITE_API_URL}/posts`, {
+    method: 'POST',
+    credentials: 'include',
+    body: JSON.stringify({ title, text, published }),
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  });
+
+  const result = await post.json();
+
+  if (!post.ok) throw new Error(result.message);
+
+  return result;
+}
+
+export async function deletePost({ postId }: { postId: string }) {
+  const post = await fetch(`${import.meta.env.VITE_API_URL}/posts/${postId}`, {
+    method: 'DELETE',
+    credentials: 'include',
+  });
+
+  const result = await post.json();
+
+  if (!post.ok) throw new Error(result.message);
+
+  return result;
+}
