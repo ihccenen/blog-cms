8d786cff425a2b3704bfe98f6916d086555a0614
ihccenen <wirkz01@gmail.com>
2023-08-31 13:18:51 -0300

    added user and user context interfaces

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,5 +1,16 @@
 /// <reference types="vite/client" />
 
+interface IUser {
+  username: string;
+  _id: string;
+}
+
+interface IUserCtx {
+  user: IUser | null;
+  login: (user: IUser) => void;
+  logout: () => void;
+}
+
 interface IPost {
   title: string;
   text: string;
