e51df40d770c49844ef8fd04325f0e6f3b4a14f4
ihccenen <wirkz01@gmail.com>
2023-08-31 13:18:05 -0300

    added logout button

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,9 +1,22 @@
+import { useContext } from 'react';
 import { Link } from 'react-router-dom';
+import { UserContext } from '../context/userContext';
 
 export default function Navbar() {
+  const { user, logout } = useContext(UserContext);
+
+  const handleClick = () => {
+    logout();
+  };
+
   return (
-    <header className="header">
+    <header className="header flex">
       <Link to="/">Home</Link>
+      {user && (
+        <button className="logout-btn" type="button" onClick={handleClick}>
+          Logout
+        </button>
+      )}
     </header>
   );
 }
